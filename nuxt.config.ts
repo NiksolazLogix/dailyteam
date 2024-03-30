76d2724d8e9a1c82b849d0226821143044752888
niksolaz <solazzo.nicola@gmail.com>
2024-03-30 14:40:41 +0000

    create basic boilerplate

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+    modules: ['nuxt-electron'],
+    electron: {
+      build: [
+        {
+          // Main-Process entry file of the Electron App.
+          entry: 'electron/main.ts',
+        },
+      ],
+    },
+  })
+  
